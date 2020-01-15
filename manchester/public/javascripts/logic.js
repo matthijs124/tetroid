b2ed508fc38ae3d131adb1eabfadcc1f33404c74
matthijs124 <matthijsdoolaar@live.nl>
2020-01-15 15:29:17 +0100

    WebSocket put into place
    Co-authored-by: Jorge Romeu <40536127+Jorgeromeu@users.noreply.github.com>

--- manchester/public/javascripts/logic.js
+++ manchester/public/javascripts/logic.js
@@ -1,2 +1,11 @@
-console.log("Evening Lagos");
-console.log("Addis Abeba");
+
+var socket = new WebSocket("ws://localhost:3000");
+            socket.onmessage = function(event){
+                document.getElementById("hello").innerHTML = event.data;
+            }
+
+            socket.onopen = function(){
+                socket.send("Hello from your other half");
+                document.getElementById("hello").innerHTML = "Sending a first message to the server ...";
+            };
+
