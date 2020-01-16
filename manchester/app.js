b8befefc68cc2fbbf29f2a0a0cffb2a1ed226758
matthijs124 <matthijsdoolaar@live.nl>
2020-01-16 13:17:52 +0100

    further state of 2 client checking
    Co-authored-by: Jorge Romeu <40536127+Jorgeromeu@users.noreply.github.com>

--- manchester/app.js
+++ manchester/app.js
@@ -5,23 +5,31 @@ var websocket = require("ws");
 var port = process.argv[2];
 var app = express();
 
-// app.use("/", function(req, res) {
-//     res.sendFile("public/splash.html", {root: "./"});
-// });
-
 var server = http.createServer(app);
 
 const wss = new websocket.Server({ server });
 
+var numClients = {
+  whatisthis: "num",
+  value: 0
+};
+
 wss.on("connection", function(ws) {
+    numClients.value++;
+    
     //let's slow down the server response time a bit to make the change visible on the client side
     setTimeout(function() {
         console.log("Connection state: "+ ws.readyState);
-        ws.send("Thanks for the message. --Your server.");
-        ws.close();
+        if (numClients.value > 1) {
+          ws.send("2 players are connected.");
+        }
         console.log("Connection state: "+ ws.readyState);
     }, 2000);
 
+    ws.send(JSON.stringify(numClients));
+
+
+    // bunch of theoretical bullshit below this -- Mr. Krebbers
     ws.on("message", function incoming(message) {
         console.log("[LOG] " + message);
     });
@@ -32,7 +40,6 @@ server.listen(port);
 app.get('/javascripts/logic.js', function (req, res) {
   res.sendFile("/javascripts/logic.js", {root: "./public"});
 });
-
 
 app.get('/game.html', function (req, res) {
   res.sendFile("game.html", {root: "./public"});
--- manchester/public/javascripts/logic.js
+++ manchester/public/javascripts/logic.js
@@ -1,11 +1,23 @@
 
 var socket = new WebSocket("ws://localhost:3000");
             socket.onmessage = function(event){
+
                 document.getElementById("hello").innerHTML = event.data;
+
+                if(JSON.parse(event.data).whatisthis === "num") {
+
+                    if (JSON.parse(event.data).value === 2) {
+                        console.log("ready to play");
+                    }
+                }
+
+
+
             }
 
             socket.onopen = function(){
-                socket.send("Hello from your other half");
+                
+                socket.send("Hello server --client");
                 document.getElementById("hello").innerHTML = "Sending a first message to the server ...";
             };
 
